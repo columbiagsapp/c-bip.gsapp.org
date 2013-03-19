3715712471c3e551187dbeec331b70f19dc7335c
Troy Conrad Therrien <troyth@gmail.com>
2013-03-19 18:04:34 -0400

    testing home page animation effects

--- sites/all/themes/framework/js/app.js
+++ sites/all/themes/framework/js/app.js
@@ -1,4 +1,4 @@
-define([ 'lib/drupal-backbone-restws' ], function() {
+define([ 'config', 'lib/drupal-backbone-restws' ], function(config) {
   var App = function() {
   };
 
@@ -6,6 +6,20 @@ define([ 'lib/drupal-backbone-restws' ], function() {
   };
 
   console.dir(Drupal.Backbone);
+
+
+
+  /////// HOME PAGE ///////
+
+  //Clicking on main menu on home page
+  $('.front #navigation .menu li a').bind('click', function(event){
+  	event.preventDefault();
+  	$('#carousel').animate({
+  		height:0
+  	}, config.PAGE_TRANSITION_TIME);
+
+  });
+
 
   return App;
 });
---
+++ sites/all/themes/framework/js/config.js
@@ -0,0 +1,8 @@
+define([], function() {
+  var config = {};
+
+  config.PAGE_TRANSITION_TIME = 500;
+
+
+  return config;
+});
